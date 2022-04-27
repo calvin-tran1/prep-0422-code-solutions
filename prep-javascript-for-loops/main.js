34d3c730628e0a1ded29dd0d5ada3a15c8d860eb
Calvin Tran <calvinxtran@gmail.com>
2022-04-27 19:39:54 +0000

    completed prep-javascript-for-loops

---
+++ prep-javascript-for-loops/main.js
@@ -0,0 +1,12 @@
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
+
+for (let i = 0; i < 20; i += 2) {
+  console.log(i);
+}
+
+for (var i = 101 - 1; i >= 0; i--) {
+  var explosion = 'Time till explosion: ' + i + '!';
+  console.log(explosion);
+}
